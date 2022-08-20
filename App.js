c8c778e29a1c1ac4f38ae48b0094772b9d8363f5
ConceitoGramatical <gabriel.belse11@gmail.com>
2022-08-20 01:44:40 +0000

    Gabriel é pessimo em html

--- App.js
+++ App.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+	const [valor1, setValor1] = useState("1");
+	const [valor2, setValor2] = useState("1000");
+	const [resultado, setResultado] = useState("");
+		function somar(){
+			let r = parseFloat(valor1) + parseFloat(valor2);
+			setResultado(r);
+		}
+	//Form f = new form();->instancia
   return (
     <View style={styles.container}>
 		  <View style={styles.bloco}>
-      		<Text style={styles.titulo}>Calculadora Simples</Text>
+			  <Text style={styles.titulo}>CALCULADORA SIMPLES</Text>
    		 </View>
 		  <View style={styles.bloco}>
 			  <Text> Valor 1: </Text>
-			  <TextInput style={styles.input}/>
+			  <TextInput style={styles.input}value={valor1} />
 		  </View>
 	      <View style={styles.bloco}>
 		     <Text> Valor 2: </Text>
-		     <TextInput style={styles.input}/>
+		     <TextInput style={styles.input}value={valor2}/>
 		  </View>
 		  <View style={styles.bloco}>
-		  	  <TouchableOpacity style={styles.botao}>
-			  	<Text styles={styles.textoBotao}>Somar</Text>
+		  	  <TouchableOpacity style={styles.botao}
+				  onPress={somar}>
+			  	<Text styles={styles.textoBotao}>SOMAR</Text>
 			  </TouchableOpacity>
 		  </View>
+		  <View style={styles.bloco}>
+			  <Text style={styles.titulo}>
+				  RESULTADO:{resultado}
+				  </Text>
+		</View>
     </View>
 	
 	);
@@ -27,13 +41,15 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  container: { 
+	backgroundColor:'#FDF5E6',
+    flex: 1
 },
 bloco:{
 	marginTop:20,
 	width:'80%',
-	marginLeft:'10%'
+	marginLeft:'10%',
+	
 },
 titulo:{
 	fontSize:30,
@@ -42,15 +58,22 @@ titulo:{
 input:{
 	borderWidth:2,
 	fontSize:20,
+	textAlign:'center',
+	borderRadius:10
 },
 botao:{
-	backgroundColor:'#000',
-	borderRadius:10
+	backgroundColor:'#32CD32',
+	borderRadius:10,
+	border:'2px solid #000',
+	textAlign:'center',
+	marginLeft:'20%',
+	marginRight:'20%'
 },
 textoBotao:{
 	color:'#fff',
 	textAlign:'center',
-	fontSize:20,
+	fontSize:'20',
+	
 },
    
  
